b44808d225e05cb5a8c189159b339e32e90e7924
anddev-rh <andresrh222@gmail.com>
2021-08-27 10:27:51 -0500

    adding home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <p>home</p>
+    <div>
+      <h1>Welcome to the trivia Challenge!</h1>
+      <p>You will be presented with 10 True or False questions.</p>
+      <p>Can you score 100%?</p>
+      <Link to="/quiz">
+        BEGIN
+      </Link>
+    </div>
+  
+
   )
 }
 
---
+++ src/context/QuizContext.jsx
@@ -0,0 +1,12 @@
+import React, {createContext, useState} from 'react';
+
+export const QuizContext = createContext(); 
+
+const QuizContextProvider = () => {
+
+
+
+
+}
+
+export default QuizContextProvider;
