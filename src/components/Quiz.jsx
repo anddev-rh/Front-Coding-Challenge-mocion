8d61d17559a0edf3c0b384f9c0242d669e1aefd2
anddev-rh <andresrh222@gmail.com>
2021-08-27 10:16:19 -0500

    adding initial components and react-router-dom

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Quiz = () => {
+  return (
+    <p>quiz</p>
+  )
+}
+
+export default Quiz;
---
+++ src/components/Results.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Results = () => {
+  return (
+    <p>Results</p>
+  )
+}
+
+export default Results;
